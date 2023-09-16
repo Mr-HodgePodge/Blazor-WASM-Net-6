bd6a70cc9d395b3fdcf622a1de160cbefb9e6183
Dylan <dylan_krajnc@tutanota.com>
2023-09-16 17:43:22 +0100

    SwaggerUI
    - added swaggerUI document functionality

--- BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace BlazorEcommerce.Server.Controllers
         };
 
         [HttpGet]
-        public async Task<IActionResult> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
             return Ok(Products);
         }
