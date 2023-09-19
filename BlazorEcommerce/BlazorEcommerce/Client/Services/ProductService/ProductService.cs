10e0f5ede9ed5fd130120a72e78ca6ff4897a077
Dylan <dylan_krajnc@tutanota.com>
2023-09-19 01:33:32 +0100

    Client Get Product
    - implemented the GetProduct endpoint on the client side

--- BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -17,5 +17,12 @@
             if(result != null && result.Data != null)
                 Products = result.Data;
         }
+
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            return result;
+        }
+
     }
 }
